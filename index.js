e1455880bc4ca569075e615de702c1a3696742ee
leeminkyu1212 <leeminkyu1212@gmail.com>
2023-03-07 14:04:40 +0900

    20230307

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
+//express 가져온다
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+//ip는 집주소 port번호는 방번호 
 const PORT = 8080;
 
 app.use(express.json());
 app.use(morgan("dev"));
 
+
+
 const cors = require("cors");
+
 app.use(cors());
 const { pool } = require("./db");
 
 
 
-
-app.get("/api/orders", async (req, res) => {
+app.get("/api/menus/:id", async (req, res) => {
   try {
-    const data = await pool.query("SELECT * FROM orders");
-    if (data[0]) {
-      return res.json(data[0]);
+    const data = await pool.query(
+      `
+        SELECT * FROM menus 
+        WHERE id=${req.params.id}
+      `
+    );
+    if (data[0][0]) {
+      return res.json(data[0][0]);
     }
   } catch (error) {
     console.log(error);
@@ -36,21 +45,6 @@ app.get("/api/menus", async (req, res) => {
   }
 });
 
-app.get("/api/menus/:id", async (req, res) => {
-  try {
-    const data = await pool.query(
-      `
-        SELECT * FROM menus 
-        WHERE menu_id=${req.params.id}
-      `
-    );
-    if (data[0][0]) {
-      return res.json(data[0][0]);
-    }
-  } catch (error) {
-    console.log(error);
-  }
-});
 
 app.post("/api/menus", async (req, res) => {
   try {
@@ -59,7 +53,7 @@ app.post("/api/menus", async (req, res) => {
         INSERT INTO menus 
         (menu_name, menu_description, menu_img_link) 
         VALUES 
-        ("${req.body.menu_name}", "${req.body.menu_description}", "${req.body.menu_img_link}")
+        ("${req.body.name}", "${req.body.description}", "${req.body.image_src}")
       `
     );
     if (data[0]) {
@@ -75,8 +69,8 @@ app.patch("/api/menus/:id", async (req, res) => {
     const data = await pool.query(
       `
         UPDATE menus 
-        SET menu_name="${req.body.menu_name}", menu_description="${req.body.menu_description}", menu_img_link="${req.body.menu_img_link}"
-        WHERE menu_id=${req.params.id}
+        SET name="${req.body.name}", menu_description="${req.body.description}", menu_img_link="${req.body.image_src}"
+        WHERE id=${req.params.id}
       `
     );
     if (data[0]) {
@@ -91,7 +85,7 @@ app.delete("/api/menus/:id", async (req, res) => {
   try {
     const data = await pool.query(
       `
-        DELETE FROM menus WHERE menu_id=${req.params.id}
+        DELETE FROM menus WHERE id=${req.params.id}
       `
     );
     if (data[0]) {
@@ -101,5 +95,90 @@ app.delete("/api/menus/:id", async (req, res) => {
     console.log(error);
   }
 });
+
+////////////////////////////////////////////////
+
+
+
+
+app.get("/api/orders", async (req, res) => {
+  try {
+    const data = await pool.query("SELECT * FROM orders");
+    if (data[0]) {
+      return res.json(data[0]);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get("/api/orders/:id", async (req, res) => {
+  try {
+    const data = await pool.query(
+      `
+        SELECT * FROM  orders
+        WHERE id=${req.params.id}
+      `
+    );
+    if (data[0][0]) {
+      return res.json(data[0][0]);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/api/orders", async (req, res) => {
+  try {
+    const data = await pool.query(
+      `
+        INSERT INTO orders 
+        (quantity, request_detail, menus_id) 
+        VALUES 
+        ("${req.body.quantity}", "${req.body.request_detail}", "${req.menus_id}")
+      `
+    );
+    if (data[0]) {
+      return res.json(data[0]);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.patch("/api/orders/:id", async (req, res) => {
+  try {
+    const data = await pool.query(
+      `
+        UPDATE orders 
+        SET quantity="${req.body.quantity}", request_detail="${req.body.request_detail}", menus_id="${req.body.menus_id}"
+        WHERE id=${req.params.id}
+      `
+    );
+    if (data[0]) {
+      return res.json(data[0]);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.delete("/api/orders/:id", async (req, res) => {
+  try {
+    const data = await pool.query(
+      `
+        DELETE FROM orders WHERE id=${req.params.id}
+      `
+    );
+    if (data[0]) {
+      return res.json(data[0]);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+
 
 app.listen(PORT, () => console.log(`this server is listening on ${PORT}`));
